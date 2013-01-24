af4e61f62d5b1b8282ec28e03aafab25017e2ff9
franciscojesusjimenez <franciscojesusjimenez@gmail.com>
2013-01-24 09:40:27 +0100

    dias laborables y jornada laboral. check de reserva con ajax

--- dospadel.py
+++ dospadel.py
@@ -6,13 +6,13 @@ import urllib
 import urllib2
 import webapp2
 import jinja2
-
+import json
 import os
-
 import paypal
 
+
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -20,6 +20,8 @@ from google.appengine.api import images                         #tratamiento de 
 from google.appengine.ext import blobstore                      #subida de archivos
 from google.appengine.ext.webapp import blobstore_handlers
 
+#######################################################################################################
+#  MODELO DE DATOS
 
 class Product(db.Model):
     """ Models an individual product """
@@ -37,9 +39,22 @@ class Reserve(db.Model):
     paykey = db.StringProperty() #paypal paykey
     status = db.StringProperty() #payment status
     created = db.DateTimeProperty(auto_now=True) #hora y fecha de creación de la reserva
- 
 
+##dias no laborables en los que no se permiten reservas
+class Holiday(db.Model):
+    """ Models an individual holiday """
+    day = db.DateProperty()
+    comment = db.StringProperty()
+
+##jornada laboral
+class Journey(db.Model):
+    hour = db.IntegerProperty()
+
+#
+########################################################################################################
+    
 class MainPage(webapp2.RequestHandler):
+    """Manejo de la pagina principal"""
     def get(self):
         user = users.get_current_user()
         if user:
@@ -102,13 +117,17 @@ class UploadProductImage(blobstore_handlers.BlobstoreUploadHandler):
         self.redirect(str('product/'+p.nm))    
 
 class ReservePage(webapp2.RequestHandler):
-    """Formalizar reservas de los productos"""
+    """Maneja las reservas de los productos"""
     def get(self,productnm):
         k=db.Key.from_path('Product',productnm)
         product=db.get(k)                                           #ejemplo de consulta por key a la bd
 
+        journey=Journey.all()
+
         template_values={
             'product': product,
+            'user': users.get_current_user(),
+            'journey': journey
         }
         template = jinja_environment.get_template('reserve.html')
         self.response.out.write(template.render(template_values))
@@ -139,13 +158,24 @@ class ReservePage(webapp2.RequestHandler):
         res.status="unknow"
         res.userid=users.get_current_user().user_id()
         res.dt=dt
+
+        ##compruebo que no existe ya una reserva a esa hora y ese dia
+        #data={  'time':time,
+        #        'date':date,
+        #        'name':product.nm,
+        #     }
+        #raw_request=json.dumps(data)
+        #url_request=urllib2.Request("http://localhost:8080/check",raw_request)
+        #raw_response=urllib2.urlopen(url_request).read()
+        #if raw_response != 0:
+        #    return
+        #else:
         res.put()
-        
         self.redirect('https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&'+urllib.urlencode({'paykey':paykey}) )
 
 
 class MyReservesPage(webapp2.RequestHandler):
-    """Detalle de reservas de cada usuario"""
+    """Maneja el detalle de reservas de cada usuario"""
     def get(self):
         reserves = db.GqlQuery("SELECT *  "
                                "FROM Reserve "
@@ -192,9 +222,11 @@ class ProductImage(webapp2.RequestHandler):
 class Transactions(webapp2.RequestHandler):
     """Recibe mensajes IPN de la API de paypal y agrega informacioon a Reserve"""
     def get(self):
-        self.request.GET.copy()
+        pass
         
     def post(self):
+        """Procesa mensajes IPN  de paypal"""
+
         ##verificar que el mensaje procede de paypal
         verify_request=urllib2.Request("https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_notify-validate&",urllib.urlencode(self.request.POST.copy()))
         verify_response = urllib2.urlopen(verify_request)
@@ -212,6 +244,66 @@ class Transactions(webapp2.RequestHandler):
         reserve.status=self.request.get('status')
         reserve.put()
 
+class Check(webapp2.RequestHandler):
+    """Comprueba disponibilidad de una reserva"""
+    def get(self):
+        pass
+    def post(self):
+        form_date=self.request.get('date')
+        form_time=self.request.get('time')
+        form_name=self.request.get('name')
+
+        d=form_date.split('-',3)
+        t=form_time.split(':',3)
+        dt=datetime.datetime(int(d[0]),int(d[1]),int(d[2]),int(t[0]))
+
+        k = db.Key.from_path('Product', form_name)
+        product=db.get(k)
+  
+        reserves = db.GqlQuery("SELECT *  "
+                               "FROM Reserve "
+                               "WHERE dt = :1 AND product = :2",
+                               dt,product)
+
+        self.response.out.write(reserves.count())
+
+class AllProductsPage(webapp2.RequestHandler):
+    """Lista todos los productos disponibles"""
+    def get(self):
+        products = Product.all()
+        template_values={
+            'products' : products,
+        }
+        template = jinja_environment.get_template('all_products.html')
+        self.response.out.write(template.render(template_values))
+
+class AdminPage(webapp2.RequestHandler):
+    """configuracion para el administrador"""
+    def get(self):
+        template_values={
+            
+            }
+        template = jinja_environment.get_template('admin.html')
+        self.response.out.write(template.render(template_values))
+    def post(self):
+        o = self.request.get('option')
+        if o == 'jl':
+            #agregar hora a jornada laboral
+            jl=Journey()
+            jl.hour=int(self.request.get('hr'))
+            jl.put()
+            return
+        if o == 'df':
+            #agregar dia festivo a jornada laboral
+            dt_string=self.request.get('dt')
+            d=dt_string.split('-',3)
+            
+            df=Holiday()
+            df.day=datetime.date(int(d[0]),int(d[1]),int(d[2]))
+            df.comment=self.request.get('comment')
+            df.put()
+            return
+        self.redirect('/admin')
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/product/(.*)/img', ProductImage),
@@ -221,5 +313,8 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                (r'/product/(.*)',ReservePage),
                                (r'/reserves/(.*)',ReserveDetailPage),
                                ('/transactions',Transactions),
+                               ('/check', Check),
+                               ('/products',AllProductsPage),
+                               ('/admin',AdminPage),
                               ],
                               debug=True)
